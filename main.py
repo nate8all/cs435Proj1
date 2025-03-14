0938f12ee51664ade2469f0b7cf61bd6c02e1908
Nathan Alleyne <nathan.alleyne7@gmail.com>
2025-03-14 13:03:04 -0400

    question 1 touchup. adult_data and test compiles, removes missing vals

--- main.py
+++ main.py
@@ -7,17 +7,23 @@ column_names = [
     "capital-loss", "hours-per-week", "native-country", "income"
 ]
 
-#define folder path where adult_data resides
+#define folder path where adult_data/test reside
 folder_path = "adult"
-file_path = os.path.join(folder_path, "adult.data")
+data_file_path = os.path.join(folder_path, "adult.data")
+test_file_path = os.path.join(folder_path, "adult.test")
 
 #load dataset, update file path
-adult_data = pd.read_csv(file_path, header = None, names = column_names, na_values = " ?")
+adult_data = pd.read_csv(data_file_path, header = None, names = column_names, na_values = " ?")
+adult_test = pd.read_csv(test_file_path, header = None, names = column_names, na_values = " ?")
 
-#manually remove rows with missing vals
+#manually remove rows with missing vals (question1)
 cleaned_data = adult_data.dropna()
+cleaned_test = adult_test.dropna()
 
 #save cleaned dataset
-cleaned_file_path = os.path.join(folder_path, "adult_cleaned.csv")
-cleaned_data.to_csv(cleaned_file_path, index=False)
-print("Cleaning complete. Missing values have been removed.")
+cleaned_data_file_path = os.path.join(folder_path, "adult_data_cleaned.csv")
+cleaned_data.to_csv(cleaned_data_file_path, index=False)
+cleaned_test_file_path = os.path.join(folder_path, "adult_test_cleaned.csv")
+cleaned_test.to_csv(cleaned_test_file_path, index=False)
+
+print("Cleaning complete. Missing values from adult_data and adult_test have been removed.")
